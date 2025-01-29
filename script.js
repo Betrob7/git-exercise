4fdbcb2c3fcb169ea477fd3351305484d165a291
Robin Lindqvist <robinlindqvist@live.se>
2025-01-29 16:36:30 +0100

    created global array and function to populate it

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log('Hello world!');
+
+let globalUsers = [];
+
+function initGlobalUsers() {
+    globalUsers.push({
+        username: 'Robin',
+        password: 'poker'
+    });
+    globalUsers.push({
+        username: 'Waldemar',
+        password: 'hockey'
+    });
+    globalUsers.push({
+        username: 'Kalinasson',
+        password: 'pingis'
+    });
+    globalUsers.push({
+        username: 'Walle',
+        password: 'lego'
+    });
+}
